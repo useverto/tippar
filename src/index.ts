2c2c5183b6d32502c68e09d8ca8dac3074f30091
tate <tateflyer1@gmail.com>
2021-01-14 19:16:53 -0500

    feat(wip): recursive contract checking

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import Arweave from "arweave";
 import { readContract, selectWeightedPstHolder } from "smartweave";
+import { tx } from "ar-gql";
 
 const client = Arweave.init({
   host: "arweave.net",
@@ -17,7 +18,9 @@ export async function chooseRecipient(
   const state = await readContract(client, contract);
   const balances = state.balances;
   const vault = state.vault;
+  let chosenRecipient: string;
 
+  // Add the liquid balance and the vault balances
   for (const addr of Object.keys(vault)) {
     if (!vault[addr].length) continue;
 
@@ -31,23 +34,53 @@ export async function chooseRecipient(
     }
   }
 
-  let chosenWallet: string = "";
-
   if (!mode || mode === "weightedRandom") {
     // Default to weighted random
+    chosenRecipient = selectWeightedPstHolder(balances);
   } else if (mode === "greatest") {
     // Choose greatest token holder
+    chosenRecipient = "";
   } else {
     throw new Error("There was a problem parsing the chooseRecipient mode");
   }
 
-  const walletBalance = client.wallets.getBalance(chosenWallet);
-  // Make sure chosen holder isn't a contract
-  return {};
+  // Check if chosen holder is a contract or wallet
+  const transactionResponse = await tx(chosenRecipient);
+
+  if (transactionResponse !== null) {
+    // Chosen user might be a SmartWeave Contract
+    let swContract: boolean = false;
+    transactionResponse.tags.forEach(tag => {
+      if (tag.name === "App-Name" && tag.value === "SmartWeaveContract")
+        swContract = true;
+    });
+
+    if (swContract) {
+      // Is SW Contract => Find new recipient inside new contract
+      try {
+        chosenRecipient = await chooseRecipient(client, chosenRecipient, mode);
+      } catch (err) {
+        throw new Error(err);
+      }
+    } else {
+      // Not SW Contract => Find new recipient for given contract
+      try {
+        chosenRecipient = await chooseRecipient(client, contract, mode);
+      } catch (err) {
+        throw new Error(err);
+      }
+    }
+  }
+  return chosenRecipient;
 }
 
-chooseRecipient(
-  client,
-  "usjm4PCxUd5mtaon7zc97-dt-3qf67yPyqgzLnLqk5A",
-  "weightedRandom"
-);
+async function testThis() {
+  const stupid = await chooseRecipient(
+    client,
+    "usjm4PCxUd5mtaon7zc97-dt-3qf67yPyqgzLnLqk5A",
+    "weightedRandom"
+  );
+  console.log("RESPONSE: " + stupid);
+}
+
+testThis();
